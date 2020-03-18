84999463877b39f89a11e10782141039b6bab416
unknown <amberj.kolar@gmail.com>
2020-03-18 11:32:15 -0700

    add workflow class References #1

--- main.rb
+++ main.rb
@@ -3,9 +3,11 @@
 #   for a week
 # So that I know how much money I will need to pay
 
+# require_relative 'bike'
 require_relative 'bmx_bike'
 require_relative 'road_bike'
 require_relative 'mountain_bike'
+require_relative 'main_workflow'
 
 bikes = [
   BmxBike.new,
@@ -14,15 +16,7 @@ bikes = [
   MountainBike.new
 ]
 
-total_price = 0
-total_price += bikes[0].weekly_rate
-total_price += bikes[0].tail_pack.cost
-total_price += bikes[1].daily_rate * 7
-total_price += bikes[1].panniers[0].price
-total_price += bikes[1].panniers[1].price
-total_price += bikes[2].weekly_rate
-total_price += bikes[2].luggage.price
-total_price += bikes[3].weekly_rate
-total_price += bikes[3].luggage.price
+workflow = MainWorkflow.new(bikes)
+workflow.run
 
-puts "Total price: #{total_price}"
+puts "Total price: #{workflow.total_price}"
